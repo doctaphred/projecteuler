c806f4eec38d7e6053b4027757f9bdc381d1294e
doctaphred <doctaphred@gmail.com>
2016-02-05 17:37:35 -0500

    Add setup.py and requirements.txt

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3 -u
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+
+setup(
+    name='projecteuler',
+    version='1.0',
+    author='Frederick Wagner',
+    url='https://github.com/doctaphred/projecteuler',
+    description='Solutions to selected Project Euler problems',
+    packages=['projecteuler'],
+    entry_points={
+        'console_scripts': ['euler = projecteuler.run:main'],
+        },
+    )
