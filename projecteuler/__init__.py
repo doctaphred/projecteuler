dc0b480afd9def5d819623cd774d542d5eca7c7f
doctaphred <doctaphred@gmail.com>
2016-02-05 20:36:49 -0500

    Update 'run.py' -> '__main__.py'

--- projecteuler/__init__.py
+++ projecteuler/__init__.py
@@ -3,7 +3,7 @@
 
 Framework for verifying and timing solutions to Project Euler problems.
 
-See run.py for the main entry point, and problems/__init__.py for
+See __main__.py for the main entry point, and problems/__init__.py for
 instructions on adding new problems.
 
 See also https://projecteuler.net/
