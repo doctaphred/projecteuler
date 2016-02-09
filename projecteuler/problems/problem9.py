be1b1c810040b217932affaaae2c33140ced4197
doctaphred <doctaphred@gmail.com>
2016-02-09 14:09:57 -0500

    Make solution to problem 9 slightly less naive

--- projecteuler/problems/problem9.py
+++ projecteuler/problems/problem9.py
@@ -1,16 +1,17 @@
+from itertools import combinations
+
+
 number = 9
 target = 1000
 answer = 31875000
 
 
 def whatever(n):
-    for a in range(1, n):
-        for b in range(a, n):
-            for c in range(b, n):
-                if (a + b + c == n and
-                        a ** 2 + b ** 2 == c ** 2):
-                    yield a * b * c
+    for a, b in combinations(range(1, n), 2):
+        c = n - a - b
+        if a ** 2 + b ** 2 == c ** 2:
+            yield a * b * c
 
 
 def solution():
-    return next(whatever(1000))
+    return next(whatever(target))
